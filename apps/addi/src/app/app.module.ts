9293eeb40bf0247c09c637a8563b25a1f0504621
Milena Suarez <msuarez@addi.com>
2019-05-02 16:51:37 -0500

    Use lib in app root

--- apps/addi/src/app/app.module.ts
+++ apps/addi/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { ClientManagementModule } from '@addi/client-management';
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ClientManagementModule],
   providers: [],
   bootstrap: [AppComponent]
 })
